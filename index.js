848acb8799da67602ada4dd5a79e94c8a90f98a6
DeividasK <deividas.karzinauskas@gmail.com>
2018-07-19 09:45:22 +0100

    Add external id

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function getListings(listingsUrls) {
     let listings = await Promise.all(listingPromises);
 
     listings = listings.reduce((acc, { listingId, listingInfo }) => {
-      return { ...acc, [listingId]: listingInfo };
+      return { ...acc, [listingId]: { externalId: listingId, ...listingInfo } };
     }, {});
 
     console.log(`listings`, listings);
@@ -59,4 +59,55 @@ async function getListing(url) {
   }
 }
 
-getListings(['https://www.zoopla.co.uk/for-sale/details/47576940']);
+getListings([
+  'https://www.zoopla.co.uk/for-sale/details/47910606'
+  // 'https://www.zoopla.co.uk/for-sale/details/47614577',
+  // 'https://www.zoopla.co.uk/for-sale/details/48141019',
+  // 'https://www.zoopla.co.uk/for-sale/details/45397791',
+  // 'https://www.zoopla.co.uk/for-sale/details/45135921',
+  // 'https://www.zoopla.co.uk/for-sale/details/47316059',
+  // 'https://www.zoopla.co.uk/for-sale/details/45587924',
+  // 'https://www.zoopla.co.uk/for-sale/details/47842027',
+  // 'https://www.zoopla.co.uk/for-sale/details/46049280',
+  // 'https://www.zoopla.co.uk/for-sale/details/48185292',
+  // 'https://www.zoopla.co.uk/for-sale/details/47409387',
+  // 'https://www.zoopla.co.uk/for-sale/details/47618627',
+  // 'https://www.zoopla.co.uk/for-sale/details/46750818',
+  // 'https://www.zoopla.co.uk/for-sale/details/48186612',
+  // 'https://www.zoopla.co.uk/for-sale/details/48239010',
+  // 'https://www.zoopla.co.uk/for-sale/details/47665573',
+  // 'https://www.zoopla.co.uk/for-sale/details/47806225',
+  // 'https://www.zoopla.co.uk/for-sale/details/45010143',
+  // 'https://www.zoopla.co.uk/for-sale/details/47648470',
+  // 'https://www.zoopla.co.uk/for-sale/details/47807537',
+  // 'https://www.zoopla.co.uk/for-sale/details/47380643',
+  // 'https://www.zoopla.co.uk/for-sale/details/47962467',
+  // 'https://www.zoopla.co.uk/for-sale/details/48326353',
+  // 'https://www.zoopla.co.uk/for-sale/details/48140623',
+  // 'https://www.zoopla.co.uk/for-sale/details/47543570',
+  // 'https://www.zoopla.co.uk/for-sale/details/47760896',
+  // 'https://www.zoopla.co.uk/for-sale/details/47576940',
+  // 'https://www.zoopla.co.uk/for-sale/details/43760134',
+  // 'https://www.zoopla.co.uk/for-sale/details/47634962',
+  // 'https://www.zoopla.co.uk/for-sale/details/47950049',
+  // 'https://www.zoopla.co.uk/for-sale/details/45723591',
+  // 'https://www.zoopla.co.uk/for-sale/details/48152449',
+  // 'https://www.zoopla.co.uk/for-sale/details/47315885',
+  // 'https://www.zoopla.co.uk/for-sale/details/47451303',
+  // 'https://www.zoopla.co.uk/for-sale/details/45186129',
+  // 'https://www.zoopla.co.uk/for-sale/details/46204835',
+  // 'https://www.zoopla.co.uk/for-sale/details/48186770',
+  // 'https://www.zoopla.co.uk/for-sale/details/45623346',
+  // 'https://www.zoopla.co.uk/for-sale/details/45086323',
+  // 'https://www.zoopla.co.uk/for-sale/details/47860424',
+  // 'https://www.zoopla.co.uk/for-sale/details/48221881',
+  // 'https://www.zoopla.co.uk/for-sale/details/47616592',
+  // 'https://www.zoopla.co.uk/for-sale/details/48104757',
+  // 'https://www.zoopla.co.uk/for-sale/details/47703819',
+  // 'https://www.zoopla.co.uk/for-sale/details/45940717',
+  // 'https://www.zoopla.co.uk/for-sale/details/46199228',
+  // 'https://www.zoopla.co.uk/for-sale/details/45786570',
+  // 'https://www.zoopla.co.uk/for-sale/details/43619171',
+  // 'https://www.zoopla.co.uk/for-sale/details/48134001',
+  // 'https://www.zoopla.co.uk/for-sale/details/45563166'
+]);
