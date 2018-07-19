313611b9b2537bf81e3554779078bd1766324aa1
DeividasK <deividas.karzinauskas@gmail.com>
2018-07-19 08:25:38 +0100

    Scrape address and geo coordinates

--- listing.js
+++ listing.js
@@ -1,5 +1,37 @@
 const { getImageNames } = require('./getImages');
 
+function getAddress(applicationJsonScriptTag) {
+  const address = applicationJsonScriptTag
+    .match(/"streetAddress": "(.+)"/gi)[1]
+    .replace('"streetAddress": "', '')
+    .replace('"', '');
+
+  const addressPartsSpace = address.split(' ');
+  const addressPartsComma = address.split(',');
+
+  const outcode = addressPartsSpace[addressPartsSpace.length - 1];
+  const borough = addressPartsComma[addressPartsComma.length - 1]
+    .replace(outcode, '')
+    .trim();
+  const street = addressPartsComma[0];
+
+  return { borough, outcode, street };
+}
+
+function getGeoCoordinates(applicationJsonScriptTag) {
+  const lat = applicationJsonScriptTag
+    .match(/"latitude": "(.+)"/gi)[0]
+    .replace('"latitude": "', '')
+    .replace('"', '');
+
+  const lng = applicationJsonScriptTag
+    .match(/"longitude": "(.+)"/gi)[0]
+    .replace('"longitude": "', '')
+    .replace('"', '');
+
+  return { lat: parseFloat(lat, 10), lng: parseFloat(lng, 10) };
+}
+
 function getListingId(url) {
   const regExp = /\/(\d+)/;
   return regExp.exec(url)[1];
@@ -7,11 +39,17 @@ function getListingId(url) {
 
 function getListingInfo(applicationJsonScriptTag) {
   return {
-    images: getImageNames(applicationJsonScriptTag)
+    images: getImageNames(applicationJsonScriptTag),
+    address: {
+      ...getAddress(applicationJsonScriptTag),
+      ...getGeoCoordinates(applicationJsonScriptTag)
+    }
   };
 }
 
 module.exports = {
+  getAddress,
+  getGeoCoordinates,
   getListingId,
   getListingInfo
 };
--- listing.test.js
+++ listing.test.js
@@ -1,14 +1,13 @@
 const APPLICATION_JSON_SCRIPT_TAG = require('./mocks/application-json-script-tag');
 
-const { getListingId, getListingInfo } = require('./listing');
+const {
+  getAddress,
+  getGeoCoordinates,
+  getListingId,
+  getListingInfo
+} = require('./listing');
 
 const zooplaUrl = 'https://www.zoopla.co.uk/for-sale/details/47576940';
-
-describe('getListingId', () => {
-  test('returns listing id given a url', () => {
-    expect(getListingId(zooplaUrl)).toBe('47576940');
-  });
-});
 
 describe('getListingInfo', () => {
   test('returns listing info', () => {
@@ -17,7 +16,39 @@ describe('getListingInfo', () => {
         '1-197dc390bc20c1f7a5c1135232f0afe36c7a17a5.png',
         '2-fcaa36eac303ea19f74c93e3eaa2f3f0d105bafc.png',
         '3-28d69406992adf99566d79b341ea2f385d2a0964.jpg'
-      ]
+      ],
+      address: {
+        borough: 'Charlton',
+        lat: 51.483893,
+        lng: 0.026893,
+        outcode: 'SE7',
+        street: 'Victoria Way'
+      }
+    });
+  });
+});
+
+describe('getListingId', () => {
+  test('returns listing id given a url', () => {
+    expect(getListingId(zooplaUrl)).toBe('47576940');
+  });
+});
+
+describe('getGeoCoordinates', () => {
+  test('returns geo coordinates', () => {
+    expect(getGeoCoordinates(APPLICATION_JSON_SCRIPT_TAG)).toMatchObject({
+      lat: 51.483893,
+      lng: 0.026893
+    });
+  });
+});
+
+describe('getAddress', () => {
+  test('returns address object', () => {
+    expect(getAddress(APPLICATION_JSON_SCRIPT_TAG)).toMatchObject({
+      street: 'Victoria Way',
+      borough: 'Charlton',
+      outcode: 'SE7'
     });
   });
 });
